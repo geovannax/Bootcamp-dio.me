0c395f20137bfb72c6bb669c7e5cc2c5c71e39d2
geovannax <assisgeovannaif@gmail.com>
2022-07-19 14:36:21 -0300

    "Adcionando resolução do exercicio"

---
+++ java/entendendo-metodos-java/exercicio02/untitled/src/br/com/dio/Main.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    quadrilateros.area(4);
+    quadrilateros.area(4,8);
+    quadrilateros.area(4,6,8);
+
+    }
+
+    }
