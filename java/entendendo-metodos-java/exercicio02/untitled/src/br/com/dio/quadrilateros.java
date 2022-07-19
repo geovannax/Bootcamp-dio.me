fa89bc51af1d2cc69b8fa8aee5fd2967d08a24eb
geovannax <assisgeovannaif@gmail.com>
2022-07-19 16:33:42 -0300

    "Adcionando resolução do exercicio 3"

--- java/entendendo-metodos-java/exercicio02/untitled/src/br/com/dio/quadrilateros.java
+++ java/entendendo-metodos-java/exercicio02/untitled/src/br/com/dio/quadrilateros.java
@@ -4,17 +4,17 @@ public class quadrilateros {
 
     public static void area(float lado){
         float areaQ=lado*lado;
-        System.out.println("Aréa do quadrado="+areaQ);
+        System.out.println("Area do quadrado="+areaQ);
     }
 
     public static void area(float base, float altura){
         float areaQ=base*altura;
-        System.out.println("Aréa do quadrado="+areaQ);
+        System.out.println("Area do quadrado="+areaQ);
     }
 
     public static void area(float baseMaior, float baseMenor, float altura){
         float areaQ=((baseMaior+baseMenor)*altura)/2;
-        System.out.println("Aréa do quadrado="+areaQ);
+        System.out.println("Area do quadrado="+areaQ);
     }
 
 }
